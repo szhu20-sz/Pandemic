9bfbd53d04912bf97a426d0a27a36b14fdba84ae
challoran20 <62893983+challoran20@users.noreply.github.com>
2020-04-10 11:34:11 -0400

    Update Healthy.java

--- Healthy.java
+++ Healthy.java
@@ -9,9 +9,6 @@ public class Healthy extends LifeForm {
 	}
 	
 	public void reproduce () {
-		int newX = (int)(Math.random()*5);
-		int newY = (int)(Math.random()*5); 
-		myWorld.getCreatureList().add(new Cow(new Location(newX,newY), myWorld));
 	}
 		
 	public void move () {
